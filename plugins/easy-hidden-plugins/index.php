3816920b867b907c9c825b0707885491bec48015
Lai Xuancheng <lai32690@gmail.com>
2019-10-12 08:20:43 -0300

    extract text_domain to variable

--- plugins/easy-hidden-plugins/index.php
+++ plugins/easy-hidden-plugins/index.php
@@ -5,22 +5,28 @@
  * Description: Hidden installed plugin from the list easily.
  */
 
-
 add_filter('all_plugins', 'hide_plugins');
 
 function hide_plugins($plugins) {
+    $TEXT_DOMAIN = 'easy-hidden-plugins';
     $current_user = wp_get_current_user();
+    $hidden_list = [ 'akismet' ];
 
     if (!($current_user->user_login === 'admin' || $current_user->ID == 1)) {
         return $plugins;
     }
 
-    $hidden_list = [ 'akismet' ];
     foreach($plugins as $key => $value) {
-        if (in_array($value[ 'TextDomain' ], $hidden_list) && is_plugin_active($key)) {
+        $text_domain = $value[ 'TextDomain' ];
+        if ($text_domain === $TEXT_DOMAIN) {
+            continue;
+        }
+
+        if (in_array($text_domain, $hidden_list) && is_plugin_active($key)) {
             unset($plugins[$key]);
         }
     }
 
     return $plugins;
 }
+
