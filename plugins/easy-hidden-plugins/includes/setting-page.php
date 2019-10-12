02d9fecb84bcc2910a8309a190a9efefd17f344a
Lai Xuancheng <lai32690@gmail.com>
2019-10-12 11:07:43 -0300

    add dashboard menu for setting page

--- plugins/easy-hidden-plugins/includes/setting-page.php
+++ plugins/easy-hidden-plugins/includes/setting-page.php
@@ -1,5 +1,24 @@
 <?php
 
+add_action('admin_menu', 'setting_page_menu_setup');
+
+function setting_page_menu_setup() {
+    add_menu_page(
+        'Easy Hidden Plugins Settings',
+        'Easy Hidden Plugins',
+        'manage_options',
+        'easy_hidden_plugins_settings',
+        'setting_page_html_render',
+        'dashicons-admin-generic'
+    );
+}
+
+function setting_page_html_render() {
+?>
+<h1>Easy Hidden Plugins Settings</h1>
+<?php
+}
+
 add_action('admin_init', 'setting_page_init');
 
 function setting_page_init() {
